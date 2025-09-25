849d94d38f846dcc14511946b428c29c96600b2c
BlueToadMakerr <140609260+BlueToadMakerr@users.noreply.github.com>
2025-09-25 17:39:16 -0400

    Update ModsPopup.cpp

--- src/ModsPopup.cpp
+++ src/ModsPopup.cpp
@@ -4,42 +4,53 @@
 
 using namespace geode::prelude;
 
-class ModsPopup : public FLAlertLayer {
+class ModsPopup : public CCNode {
 protected:
+    CCNode* m_bg = nullptr;
     CCScrollView* m_scroll = nullptr;
     CCNode* m_contentNode = nullptr;
+    CCMenu* m_menu = nullptr;
 
     bool init() {
         log::info("ModsPopup: init called");
 
-        // Make popup wider and taller: width = 500, height = 400
+        // Fullscreen transparent background
+        m_bg = CCLayerColor::create({0, 0, 0, 180});
+        this->addChild(m_bg);
+
+        // Popup background
         float popupWidth = 500.f;
         float popupHeight = 400.f;
+        m_bg = CCScale9Sprite::create("square02_001.png");
+        m_bg->setContentSize({popupWidth, popupHeight});
+        m_bg->setPosition({0, 0}); // center
+        this->addChild(m_bg, 1);
 
-        if (!FLAlertLayer::init(0, "Installed Mods", "", "OK", nullptr, popupWidth, false, popupHeight, 1.f)) {
-            log::info("ModsPopup: FLAlertLayer::init failed");
-            return false;
-        }
-
-        log::info("ModsPopup: creating scroll view");
-
-        // Scroll view slightly smaller than popup to leave padding
-        const float scrollW = popupWidth - 40.f;
-        const float scrollH = popupHeight - 80.f;
+        // Scroll view for mods
+        float scrollW = popupWidth - 40.f;
+        float scrollH = popupHeight - 80.f;
 
         m_contentNode = CCNode::create();
         m_contentNode->setContentSize({scrollW, 0});
 
         m_scroll = CCScrollView::create({scrollW, scrollH}, m_contentNode);
         m_scroll->setDirection(kCCScrollViewDirectionVertical);
-
-        // Center the scroll inside the popup
-        m_scroll->setPosition({(popupWidth - scrollW) / 2 - popupWidth / 2, (popupHeight - scrollH) / 2 - popupHeight / 2});
-
+        m_scroll->setPosition({-scrollW / 2, -scrollH / 2}); // center in bg
         m_scroll->setBounceable(true);
+        m_bg->addChild(m_scroll, 10);
 
-        // Ensure scroll is above the main layer content
-        this->m_mainLayer->addChild(m_scroll, 10);
+        // Menu for Done button
+        m_menu = CCMenu::create();
+        m_menu->setPosition({0,0});
+        m_bg->addChild(m_menu, 20);
+
+        auto doneBtn = CCMenuItemLabel::create(
+            CCLabelBMFont::create("Done", "goldFont.fnt"),
+            this,
+            menu_selector(ModsPopup::onDone)
+        );
+        doneBtn->setPosition({0, -popupHeight/2 + 30});
+        m_menu->addChild(doneBtn);
 
         populateMods(scrollW, scrollH);
 
@@ -56,7 +67,7 @@ protected:
         log::info("ModsPopup: {} mods returned by Loader", mods.size());
 
         float contentHeight = mods.size() * (itemH + padY);
-        if (contentHeight < scrollH) contentHeight = scrollH; // ensure scroll fills view
+        if (contentHeight < scrollH) contentHeight = scrollH;
 
         m_contentNode->setContentSize({width, contentHeight});
 
@@ -96,30 +107,31 @@ protected:
         }
 
         m_scroll->setContentSize({width, contentHeight});
-        m_scroll->setContentOffset({0, 0}); // scroll starts at top
-
+        m_scroll->setContentOffset({0, 0});
         log::info("ModsPopup: populateMods finished, content height {}", contentHeight);
+    }
+
+    void onDone(CCObject*) {
+        log::info("ModsPopup: Done pressed, removing popup");
+        this->removeFromParentAndCleanup(true);
     }
 
 public:
     static ModsPopup* create() {
-        log::info("ModsPopup: create called");
         auto ret = new ModsPopup();
         if (ret && ret->init()) {
             ret->autorelease();
-            log::info("ModsPopup: create success");
             return ret;
         }
         CC_SAFE_DELETE(ret);
-        log::info("ModsPopup: create failed");
         return nullptr;
     }
 
-    static void showPopup() {
-        log::info("ModsPopup: showPopup called");
+    static void show() {
         auto popup = ModsPopup::create();
-        if (popup) log::info("ModsPopup: popup created, showing");
-        else log::info("ModsPopup: popup creation failed");
-        if (popup) popup->show();
+        if (!popup) return;
+
+        auto scene = CCDirector::sharedDirector()->getRunningScene();
+        if (scene) scene->addChild(popup, 9999); // high z-order
     }
 };
