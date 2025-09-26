941e92f6b44ff77ca754ff98b15c27eb4b84d03d
BlueToadMakerr <140609260+BlueToadMakerr@users.noreply.github.com>
2025-09-26 08:15:38 -0400

    Update ModsPopup.cpp

--- src/ModsPopup.cpp
+++ src/ModsPopup.cpp
@@ -1,158 +1,79 @@
 #include <Geode/Geode.hpp>
+#include <Geode/ui/Popup.hpp>
 #include <Geode/loader/Loader.hpp>
-#include <Geode/loader/Mod.hpp>
-#include <Geode/ui/TextArea.hpp>
-#include <Geode/ui/ButtonSprite.hpp>
-#include <Geode/ui/Anchor.hpp>
+#include <Geode/ui/TextInput.hpp>
 
 using namespace geode::prelude;
 
-class ModsLayer : public CCLayer {
+class ModsPopup : public Popup<> {
 protected:
-    CCNode* m_mainLayer = nullptr;
-    CCScrollView* m_scroll = nullptr;
-    CCNode* m_contentNode = nullptr;
-    CCMenu* m_buttonMenu = nullptr;
+    ScrollLayer* m_scroll = nullptr;
+    TextInput* m_searchBar = nullptr;
+    std::vector<Mod*> m_mods;
 
-    float popupWidth = 450.f;
-    float popupHeight = 300.f;
-    float padding = 10.f;
-    float doneButtonHeight = 50.f;
+    bool setup() override {
+        this->setTitle("Installed Mods");
 
-    bool init() {
-        auto director = CCDirector::sharedDirector();
-        auto winSize = director->getWinSize();
-        auto visibleOrigin = director->getVisibleOrigin();
-
-        m_mainLayer = CCNode::create();
-        this->addChild(m_mainLayer);
-
-        // Popup background
-        auto bg = CCScale9Sprite::create("square02_001.png");
-        bg->setContentSize({popupWidth, popupHeight});
-        bg->setPosition({visibleOrigin.x + winSize.width/2, visibleOrigin.y + winSize.height/2});
-        m_mainLayer->addChild(bg);
-
-        // Scrollable mods list
-        float scrollW = popupWidth - 40.f;
-        float scrollH = popupHeight - doneButtonHeight - 3*padding;
-
-        m_contentNode = CCNode::create();
-        m_contentNode->setContentSize({scrollW, 0});
-
-        m_scroll = CCScrollView::create({scrollW, scrollH}, m_contentNode);
-        m_scroll->setDirection(kCCScrollViewDirectionVertical);
-        m_scroll->setBounceable(true);
-        m_scroll->setPosition({
-            bg->getPositionX(),
-            bg->getPositionY() - popupHeight/2 + doneButtonHeight + 2*padding
+        // --- Search bar at top ---
+        m_searchBar = TextInput::create(250.f, "Search mods...");
+        m_searchBar->setCallback([this](std::string const& text) {
+            this->populateMods(text);
         });
-        m_mainLayer->addChild(m_scroll, 10);
+        m_mainLayer->addChildAtPosition(m_searchBar, Anchor::Top, {0, -40});
 
-        populateMods(scrollW, scrollH);
+        // --- Scroll list ---
+        m_scroll = ScrollLayer::create({300.f, 180.f});
+        m_scroll->setAnchorPoint({0.5f, 0.5f});
+        m_scroll->setPosition(m_mainLayer->getContentSize() / 2);
+        m_mainLayer->addChild(m_scroll);
 
-        // Button menu
-        m_buttonMenu = CCMenu::create();
-        m_buttonMenu->setPosition({0,0});
-        m_mainLayer->addChild(m_buttonMenu, 20);
+        // Populate initially
+        this->populateMods("");
 
-        auto okSpr = ButtonSprite::create("OK", "goldFont.fnt", "GJ_button_01.png", 0.8f);
-        auto okBtn = CCMenuItemSpriteExtra::create(
-            okSpr, this, menu_selector(ModsLayer::onDone)
+        // --- Done button ---
+        auto doneSpr = ButtonSprite::create("Done", "goldFont.fnt", "GJ_button_01.png", 0.8f);
+        auto doneBtn = CCMenuItemSpriteExtra::create(
+            doneSpr, this, menu_selector(ModsPopup::onClose)
         );
-
-        okBtn->setPosition({
-            bg->getPositionX(),                  // center horizontally
-            bg->getPositionY() - popupHeight/2 + doneButtonHeight/2 + padding // bottom middle
-        });
-
-        m_buttonMenu->addChild(okBtn);
-
-        // Enable touches to block background clicks
-        setTouchEnabled(true);
-        setTouchMode(kCCTouchesOneByOne);
-        setTouchPriority(-128);
+        m_buttonMenu->addChildAtPosition(doneBtn, Anchor::Bottom, {0, 25});
 
         return true;
     }
 
-    void populateMods(float width, float scrollH) {
-        const float itemH = 50.f;
-        const float padY = 8.f;
+    void populateMods(std::string const& filter) {
+        m_scroll->m_contentLayer->removeAllChildren();
 
-        auto mods = Loader::get()->getAllMods();
-        float contentHeight = mods.size() * (itemH + padY);
-        if (contentHeight < scrollH) contentHeight = scrollH;
-
-        m_contentNode->setContentSize({width, contentHeight});
-
-        float y = contentHeight;
-        for (auto* mod : mods) {
-            if (!mod) continue;
-
-            auto itemBG = CCScale9Sprite::create("square01_001.png");
-            itemBG->setContentSize({width, itemH});
-            itemBG->setPosition({width/2, y - itemH/2});
-            m_contentNode->addChild(itemBG);
-
-            auto lbl = CCLabelBMFont::create(mod->getName().c_str(), "goldFont.fnt");
-            lbl->setAnchorPoint({0.f, 0.5f});
-            lbl->setPosition({12.f, y - itemH/2});
-            lbl->setScale(0.5f);
-            m_contentNode->addChild(lbl);
-
-            auto ver = CCLabelBMFont::create(mod->getVersion().toVString().c_str(), "chatFont.fnt");
-            ver->setAnchorPoint({1.f, 0.5f});
-            ver->setPosition({width - 12.f, y - itemH/2});
-            ver->setScale(0.45f);
-            m_contentNode->addChild(ver);
-
-            y -= itemH + padY;
+        m_mods.clear();
+        for (auto& mod : Loader::get()->getAllMods()) {
+            if (filter.empty() || mod->getName().find(filter) != std::string::npos) {
+                m_mods.push_back(mod);
+            }
         }
 
-        if (mods.empty()) {
-            auto lbl = CCLabelBMFont::create("No mods installed.", "chatFont.fnt");
-            lbl->setPosition({width/2, 20});
-            m_contentNode->addChild(lbl);
+        float y = 0.f;
+        for (auto* mod : m_mods) {
+            auto label = CCLabelBMFont::create(
+                fmt::format("{} v{}", mod->getName(), mod->getVersion().toVString()).c_str(),
+                "chatFont.fnt"
+            );
+            label->setAnchorPoint({0, 1});
+            label->setPosition({5, -y});
+            label->setScale(0.6f);
+            m_scroll->m_contentLayer->addChild(label);
+
+            y += 22.f;
         }
 
-        m_scroll->setContentSize({width, contentHeight});
-        m_scroll->setContentOffset({0,0});
+        m_scroll->m_contentLayer->setContentSize({300.f, y});
+        m_scroll->moveToTop();
     }
 
-    void onDone(CCObject*) {
-        this->removeFromParentAndCleanup(true);
-    }
-
-    virtual bool ccTouchBegan(CCTouch* touch, CCEvent* event) override {
-        auto loc = touch->getLocation();
-        auto bgPos = m_mainLayer->getPosition();
-        auto bgSize = CCSize(popupWidth, popupHeight);
-
-        // Block touches outside popup
-        if (loc.x < bgPos.x - popupWidth/2 || loc.x > bgPos.x + popupWidth/2 ||
-            loc.y < bgPos.y - popupHeight/2 || loc.y > bgPos.y + popupHeight/2) {
-            return true;
-        }
-        return false;
+    void onClose(CCObject*) {
+        this->onClosePopup();
     }
 
 public:
-    static ModsLayer* create() {
-        auto ret = new ModsLayer();
-        if (ret && ret->init()) {
-            ret->autorelease();
-            return ret;
-        }
-        CC_SAFE_DELETE(ret);
-        return nullptr;
-    }
-
     static void showPopup() {
-        auto popup = ModsLayer::create();
-        if (!popup) return;
-
-        auto scene = CCDirector::sharedDirector()->getRunningScene();
-        if (scene) scene->addChild(popup, 9999);
+        ModsPopup::create(350.f, 300.f)->show();
     }
 };
