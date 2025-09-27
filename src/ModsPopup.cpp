b21a62c3e5c5d2e1e1bcbb04b9c32ac6b4504ffe
BlueToadMakerr <140609260+BlueToadMakerr@users.noreply.github.com>
2025-09-27 13:16:16 -0400

    Update ModsPopup.cpp

--- src/ModsPopup.cpp
+++ src/ModsPopup.cpp
@@ -145,7 +145,7 @@ protected:
                 m_modStates[modID] = !m_modStates[modID];
                 toggleBtnSpr->setString(m_modStates[modID] ? "Enabled" : "Disabled");
             });
-            toggleBtn->setPosition({ -150.f, 0.f }); // top
+            toggleBtn->setPosition({ -50.f, 0.f }); // top
             menu->addChild(toggleBtn);
 
             // View button (bottom)
@@ -153,7 +153,7 @@ protected:
             auto viewBtn = CCMenuItemExt::createSpriteExtra(viewBtnSpr, [mod](CCObject*) {
                 geode::openInfoPopup(mod->getID());
             });
-            viewBtn->setPosition({ 0.f, 0.f }); // bottom
+            viewBtn->setPosition({ 15.f, 0.f }); // bottom
             menu->addChild(viewBtn);
 
             item->addChild(menu);
