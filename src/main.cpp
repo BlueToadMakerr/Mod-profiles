4733d7937ed5625696817802212782ead0a9b73b
BlueToadMakerr <140609260+BlueToadMakerr@users.noreply.github.com>
2025-10-05 16:18:05 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,82 +4,39 @@
 
 using namespace geode::prelude;
 
-// --- MenuLayer modification ---
 class $modify(MyMenuLayer, MenuLayer) {
-    bool init() {
-        log::info("MyMenuLayer: init called");
+bool init() {
+log::info("MyMenuLayer: init called");
 
-        if (!MenuLayer::init()) {
-            log::info("MyMenuLayer: MenuLayer::init failed");
-            return false;
-        }
+if (!MenuLayer::init()) {  
+        log::info("MyMenuLayer: MenuLayer::init failed");  
+        return false;  
+    }  
 
-        log::info("MyMenuLayer: creating mods button");
+    log::info("MyMenuLayer: creating mods button");  
 
-        // Create mods button
-        auto myButton = CCMenuItemSpriteExtra::create(
-            CCSprite::createWithSpriteFrameName("GJ_optionsBtn02_001.png"),
-            this,
-            menu_selector(MyMenuLayer::onMyButton)
-        );
-        myButton->setID("mods-button"_spr);
+    auto myButton = CCMenuItemSpriteExtra::create(    
+        CCSprite::createWithSpriteFrameName("GJ_optionsBtn02_001.png"),    
+        this,    
+        menu_selector(MyMenuLayer::onMyButton)    
+    );    
 
-        // Find bottom menu
-        auto menu = this->getChildByID("bottom-menu");
-        if (!menu) {
-            log::info("MyMenuLayer: bottom-menu node not found");
-        } else {
-            log::info("MyMenuLayer: bottom-menu found, adding button");
-            menu->addChild(myButton);
-            menu->updateLayout();
-            log::info("MyMenuLayer: button added and layout updated");
-        }
+    auto menu = this->getChildByID("bottom-menu");    
+    if (!menu) log::info("MyMenuLayer: bottom-menu node not found");    
+    else log::info("MyMenuLayer: bottom-menu found, adding button");  
 
-        return true;
-    }
+    menu->addChild(myButton);    
+    myButton->setID("mods-button"_spr);    
 
-    void onMyButton(CCObject*) {
-        log::info("MyMenuLayer: mods button pressed");
-        ModsPopup::showPopup();
-    }
-};
+    menu->updateLayout();    
+    log::info("MyMenuLayer: button added and layout updated");  
 
-// --- ModsLayer modification ---
-class $modify(MyModsLayer, ModsLayer) {
-    bool init(ModListType type) {
-        log::info("MyModsLayer: init called");
+    return true;    
+}    
 
-        if (!ModsLayer::init(type)) {
-            log::info("MyModsLayer: ModsLayer::init failed");
-            return false;
-        }
+void onMyButton(CCObject*) {    
+    log::info("MyMenuLayer: mods button pressed");    
+    ModsPopup::showPopup();    
+}
 
-        log::info("MyModsLayer: creating mods button for action-menu");
-
-        // Create mods button
-        auto myButton = CCMenuItemSpriteExtra::create(
-            CCSprite::createWithSpriteFrameName("GJ_optionsBtn02_001.png"),
-            this,
-            menu_selector(MyModsLayer::onMyButton)
-        );
-        myButton->setID("mods-button"_spr);
-
-        // Find the action-menu in ModsLayer
-        auto actionMenu = this->getChildByID("action-menu");
-        if (!actionMenu) {
-            log::info("MyModsLayer: action-menu node not found");
-        } else {
-            log::info("MyModsLayer: action-menu found, adding button");
-            actionMenu->addChild(myButton);
-            actionMenu->updateLayout();
-            log::info("MyModsLayer: button added and layout updated");
-        }
-
-        return true;
-    }
-
-    void onMyButton(CCObject*) {
-        log::info("MyModsLayer: mods button pressed");
-        ModsPopup::showPopup();
-    }
 };
